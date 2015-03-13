9b33a7dc2744ec70a1a7ac464aab620fbff87d69
yeongtaek Hong <mozodev@cckorea.org>
2015-03-13 18:38:45 +0900

    연락하기 수정

--- js/contact_me_static.js
+++ js/contact_me_static.js
@@ -15,6 +15,28 @@ $(function() {
         e.preventDefault();
         $(this).tab("show");
     });
+
+    $('#contactForm').on('submit', function(e){
+        e.preventDefault();
+        $.ajax({
+            url: "//formspree.io/creative@cckorea.org",
+            method: "POST",
+            data: {
+                name: $('#name').val(),
+                _replyto: $('#email').val(),
+                _subject: 'CCL 한국 런칭 10주년! 관련 문의',
+                message: $('#message').val(),
+                _gotcha: ''
+            },
+            dataType: "json"
+        })
+        .done(function() {
+            $('#success').html('메시지를 전달했습니다.');
+        })
+        .fail(function() {
+            $('#failed').html('음... 문제가 있네요. creative@2cckorea.org로 메일 주세요~');
+        })
+    });
 });
 
 /*When clicking on Full hide fail/success boxes */
